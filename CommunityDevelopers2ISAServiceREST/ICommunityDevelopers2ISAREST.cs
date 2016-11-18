b6bef76c7af3bfa88be3cbc24db43dfb702a8174
Hichem <hichem.tebbouche@2isa.org>
2016-11-18 11:03:21 +0100

    modif webservice

--- CommunityDevelopers2ISAServiceREST/ICommunityDevelopers2ISAREST.cs
+++ CommunityDevelopers2ISAServiceREST/ICommunityDevelopers2ISAREST.cs
@@ -26,25 +26,6 @@ namespace CommunityDevelopers2ISAServiceREST
     }
 
 
-    //// Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
-    //[DataContract]
-    //public class CompositeType
-    //{
-    //    bool boolValue = true;
-    //    string stringValue = "Hello ";
-
-    //    [DataMember]
-    //    public bool BoolValue
-    //    {
-    //        get { return boolValue; }
-    //        set { boolValue = value; }
-    //    }
-
-    //    [DataMember]
-    //    public string StringValue
-    //    {
-    //        get { return stringValue; }
-    //        set { stringValue = value; }
-    //    }
+    
 }
 
