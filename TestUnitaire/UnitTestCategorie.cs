11d3db0f264f2e63fda75c8f01e519625898d130
Hichem <hichem.tebbouche@2isa.org>
2017-01-03 08:53:41 +0100

    creation test unit pour categorie

---
+++ TestUnitaire/UnitTestCategorie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommunityDevelopers2ISAMetiers;
+using CommunityDevelopers2ISADLL;
+
+namespace CommunityDevelopers2ISATestUnitaire
+{
+    /// <summary>
+    /// Description résumée pour UnitTestCategorie
+    /// </summary>
+    [TestClass]
+    public class UnitTestCategorie
+    {
+        [TestMethod]
+        public void TestCreationCategorie()
+        {
+            string expectedString = "Categorie de test";
+            Categorie categorie = new Categorie(1, "Categorie de test");
+
+            Assert.AreEqual(categorie.Libelle, expectedString);
+        }
+        [TestMethod]
+        public void TestGetCategorieByID()
+        {
+            string expectedString = "Adresse Utiles";
+            Categorie categorie = Outils.GetCategorieByID(2);
+
+            Assert.AreEqual(categorie.Libelle, expectedString);
+        }
+    }
+}
