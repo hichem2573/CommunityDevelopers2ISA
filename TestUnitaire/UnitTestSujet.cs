2370ed01e51c43d75e1477b8cbfa64c646c4449f
Hichem <hichem.tebbouche@2isa.org>
2017-01-13 11:09:32 +0100

    test sujets d'une catégorie

--- TestUnitaire/UnitTestSujet.cs
+++ TestUnitaire/UnitTestSujet.cs
@@ -23,18 +23,20 @@ namespace CommunityDevelopers2ISATestUnitaire
 
             // la requête doit renvoyé 1 sujet dont l'id 16 est et le titre "Sortie resto"
             Sujet sujAtt = new Sujet();
+            sujAtt.Id = 5;
 
-            sujAtt.Id = 16;
+            Sujet sujAtt1 = new Sujet();
+            sujAtt1.Id = 1;
 
             // on ajoute le sujet attendu dans la liste des sujets attendu
             sujetsAtt.Add(sujAtt);
+            sujetsAtt.Add(sujAtt1);
 
             // On instancie une liste de sujet qui va stocker les sujets retournés par la méthode de la classe Outils GetSujetsByCategorieID
             List<Sujet> sujetsRet = new List<Sujet>();
 
             // Appelle de la méthode en passant en paramétre l'id de la catégorie
-            sujetsRet = Outils.GetSujetsByCategorieID(6);
-
+            sujetsRet = Outils.GetSujetsByCategorieID(7);
             CollectionAssert.AreEqual(sujetsAtt, sujetsRet);
         }
     }
